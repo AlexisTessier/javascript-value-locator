c2ce1866c84e3ddeaf6c0c196852944651182a19
Alexis Tessier <alexistessier@MacBook-Pro-de-Alexis.local>
2017-05-29 20:35:57 +0200

    update release script

--- deployment/release.js
+++ deployment/release.js
@@ -7,12 +7,23 @@ const shell = require('shelljs');
 const pkg = require('../package.json');
 const git = require('git-repo-info');
 
-shell.exec(`git checkout master`);
+shell.exec(`git checkout release`);
+assert(git().branch === 'release');
 
+if(shell.exec(`git pull origin release`).code !== 0) {
+	shell.echo('Error: Release failed at update release branch');
+	shell.exit(1);
+}
+
+shell.exec(`git checkout master`);
 assert(git().branch === 'master');
 
-if (shell.exec(`git merge release`).code !== 0) {
-	shell.echo('Error: Release failed at merge release to master step');
+if(shell.exec(`git pull origin master`).code !== 0) {
+	shell.echo('Error: Release failed at update master branch');
+	shell.exit(1);
+}
+else if(shell.exec(`git merge release`).code !== 0) {
+	shell.echo('Error: Release failed at master update merging release');
 	shell.exit(1);
 }
 else if (shell.exec(`npm run build`).code !== 0) {
@@ -23,38 +34,54 @@ else if(shell.exec(`npm test`).code !== 0) {
 	shell.echo('Error: Release Tests failed on master');
 	shell.exit(1);
 }
-else{
-	//add the generated files which are different depending on the branch (like README.md)
-	shell.exec(`git add . && git commit -a -m "Auto-commit : pre-release ${pkg.version}"`);
+else if(shell.exec(`git add . && git commit -a -m "Auto-commit : pre-release ${pkg.version}"`).code !== 0) {
+	shell.echo('Error: Release failed at pre-release commit');
+	shell.exit(1);
+}
+else if(shell.exec(`git push origin master`).code !== 0) {
+	shell.echo('Error: Release failed at push on master');
+	shell.exit(1);
+}
 
-	//then
-	shell.exec(`git push origin master`);
+shell.exec(`git checkout release`);
+assert(git().branch === 'release');
 
-	shell.exec(`git checkout release`);
+if (shell.exec(`git merge master`).code !== 0) {
+	shell.echo('Error: Release failed at merge master step');
 
-	assert(git().branch === 'release');
+	shell.exec(`git checkout master`);
+	assert(git().branch === 'master');
 
-	if (shell.exec(`git merge -s theirs master`).code !== 0) {
-		shell.echo('Error: Release failed at merge master step');
-		shell.exit(1);
-	}
-	else if (shell.exec(`npm run build`).code !== 0) {
-		shell.echo('Error: Release build failed');
-		shell.exit(1);
-	}
-	else if(shell.exec(`npm test`).code !== 0) {
-		shell.echo('Error: Release Tests failed');
-		shell.exit(1);
-	}
-	else{
-		//add the generated files which are different depending on the branch (like README.md)
-		shell.exec(`git add . && git commit -a -m "Auto-commit : release ${pkg.version}"`);
+	shell.exit(1);
+}
+else if (shell.exec(`npm run build`).code !== 0) {
+	shell.echo('Error: Release build failed');
 
-		//then
-		shell.exec(`git push origin release`);
-	}
+	shell.exec(`git checkout master`);
+	assert(git().branch === 'master');
+
+	shell.exit(1);
+}
+else if(shell.exec(`npm test`).code !== 0) {
+	shell.echo('Error: Release Tests failed');
+
+	shell.exec(`git checkout master`);
+	assert(git().branch === 'master');
+
+	shell.exit(1);
+}
+else if(shell.exec(`git add . && git commit -a -m "Auto-commit : release ${pkg.version}"`).code !== 0) {
+	shell.echo('Error: Release failed at release commit');
+
+	shell.exec(`git checkout master`);
+	assert(git().branch === 'master');
+
+	shell.exit(1);
+}
+else if(shell.exec(`git push origin release`).code !== 0){
+	shell.echo('Error: Release failed at push on release');
+	shell.exit(1);
 }
 
 shell.exec(`git checkout master`);
-
 assert(git().branch === 'master');
